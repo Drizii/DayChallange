86efae072f359fa5bf0fbdceb833e4261d11c1ce
Drizi <wojtas1478@gmail.com>
2020-07-16 19:31:01 +0200

    models - praca nad date today

--- challange/challange/admin.py
+++ challange/challange/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Song, Person
+from .models import Song, Person, Day, Event
 
 
 admin.site.register(Person)
 admin.site.register(Song)
+admin.site.register(Day)
+admin.site.register(Event)
--- challange/challange/models.py
+++ challange/challange/models.py
@@ -1,32 +1,36 @@
+from datetime import date
 from django.db import models
 
 
-class Edition:
-    SongNameByLetter = 1
-    ArtistNameByLetter = 2
-    Choice = {
-        SongNameByLetter: "Piosenka na literę...",
-        ArtistNameByLetter: "Artysta na literę...",
-    }
+class Day(models.Model):
+    day = models.DateField(date.today)
+    condition = models.CharField(verbose_name="Warunek", max_length=64, default="wybierz")
+
+    def __str__(self):
+        return self.day
+
+
+class Event(models.Model):
+    edition = models.CharField(max_length=128)
+    eventday = models.OneToOneField(Day, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.edition
 
 
 class Person(models.Model):
-    name = models.CharField(verbose_name="Imie i nazwisko", max_length=111)
+    name = models.CharField(verbose_name="Imie i nazwisko", max_length=128)
 
     def __str__(self):
         return self.name
 
 
-class Day(models.Model):
-    day = models.DateField()
-
-
 class Song(models.Model):
-    title = models.CharField(verbose_name="Tytuł", max_length=111)
-    artist = models.CharField(verbose_name="Autor", max_length=111)
+    title = models.CharField(verbose_name="Tytuł", max_length=128)
+    artist = models.CharField(verbose_name="Autor", max_length=128)
     link = models.URLField(verbose_name="Link do YT", db_index=True, unique=True, blank=True)
     person = models.ManyToManyField(Person, verbose_name="Kto zaproponowal")
-    edition = models.PositiveSmallIntegerField(choices=Edition.Choice.items(), verbose_name="Która edycja")
+    event = models.ManyToManyField(Event, verbose_name="Która edycja", default="wybierz")
 
     def __str__(self):
         return self.artist + ' - ' + self.title
