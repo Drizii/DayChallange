5c58a0a281ade2012527fade660584e1049c1049
Drizi <wojtas1478@gmail.com>
2020-07-28 18:55:50 +0200

    zabawa z search

--- challange/challange/urls.py
+++ challange/challange/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import SongListView, SongSearchView
+from .views import SongListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', SongListView.as_view(template_name="main.html")),
-    path('search/', SongSearchView.as_view(), name='search_results')
+    path('', SongListView.as_view(template_name = "main.html"), name = 'search_results'),
+    #path('search/', SongSearchView.as_view(), name='search_results')
 ]
--- challange/challange/views.py
+++ challange/challange/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from .models import Song
 from django.views.generic import ListView
 
@@ -9,14 +10,28 @@ class SongListView(ListView):
     template_name = 'main.html'
     context_object_name = 'song_list'
 
+    def search_song_view(self, request):
+        query = request.GET.get("q", None)
+        qs = Song.objects.all()
+        if query is not None:
+            qs = qs.filter(
+                Q(title__icontains=query) | Q(artist__icontains=query)
+            )
+        context = {
+            "object_list": qs,
+        }
+        return render(request, context)
 
+'''
 class SongSearchView(ListView):
     model = Song
     template_name = 'search_results.html'
 
     def get_queryset(self):
-        query = self.request.GET.get('q')
+        query = self.request.GET.get('q', None)
         return Song.objects.filter(
             Q(title__icontains=query) | Q(artist__icontains=query)
         )
-        return object_list
+        if query is None:
+            return object_list
+'''
