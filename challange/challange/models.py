11a72f860a0a17ee2495c06d907e237e2605dc16
Drizi <wojtas1478@gmail.com>
2020-07-28 15:44:33 +0200

    xd

--- challange/challange/models.py
+++ challange/challange/models.py
@@ -17,7 +17,7 @@ class Person(models.Model):
 
 
 class Song(models.Model):
-    person = models.ManyToManyField(Person, verbose_name="Kto zaproponowal")
+    person = models.ForeignKey(Person, on_delete = models.CASCADE, verbose_name="Kto zaproponowal", default=None, blank=True)
     title = models.CharField(verbose_name="Tytuł", max_length=128)
     artist = models.CharField(verbose_name="Autor", max_length=128)
     link = models.URLField(verbose_name="Link do YT", db_index=True, unique=True, blank=True)
@@ -27,10 +27,10 @@ class Song(models.Model):
 
 
 class Day(models.Model):
-    event = models.ManyToManyField(Event, verbose_name="Która edycja", default="wybierz")
-    day = models.DateField(date.today)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name="Która edycja", default=None, blank=True)
+    day = models.DateField(default=date.today)
     condition = models.CharField(verbose_name="Warunek", max_length=64, default="wybierz")
-    song = models.ForeignKey('Song', on_delete=models.CASCADE, default=None)
+    song = models.ForeignKey('Song', on_delete=models.CASCADE, default=None, null=True)
 
     def __str__(self):
         return self.day
