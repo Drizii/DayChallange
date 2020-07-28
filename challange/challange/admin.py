482036e6ab3291d6f9e1e9db9032115582251892
Drizi <wojtas1478@gmail.com>
2020-07-28 15:18:17 +0200

    TabularInLine

--- challange/challange/admin.py
+++ challange/challange/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from .models import Song, Person, Day, Event
 
 
+class SongTabularInLine(admin.TabularInline):
+    model = Song
+
+
+class DayAdmin(admin.ModelAdmin):
+    inlines = [SongTabularInLine]
+
+    class Meta:
+        model = Day
+
+
 admin.site.register(Person)
 admin.site.register(Song)
 admin.site.register(Day)
--- challange/challange/models.py
+++ challange/challange/models.py
@@ -2,17 +2,8 @@ from datetime import date
 from django.db import models
 
 
-class Day(models.Model):
-    day = models.DateField(date.today)
-    condition = models.CharField(verbose_name="Warunek", max_length=64, default="wybierz")
-
-    def __str__(self):
-        return self.day
-
-
 class Event(models.Model):
     edition = models.CharField(max_length=128)
-    eventday = models.OneToOneField(Day, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.edition
@@ -26,12 +17,24 @@ class Person(models.Model):
 
 
 class Song(models.Model):
+    person = models.ManyToManyField(Person, verbose_name="Kto zaproponowal")
     title = models.CharField(verbose_name="Tytuł", max_length=128)
     artist = models.CharField(verbose_name="Autor", max_length=128)
     link = models.URLField(verbose_name="Link do YT", db_index=True, unique=True, blank=True)
-    person = models.ManyToManyField(Person, verbose_name="Kto zaproponowal")
-    event = models.ManyToManyField(Event, verbose_name="Która edycja", default="wybierz")
 
     def __str__(self):
         return self.artist + ' - ' + self.title
+
+
+class Day(models.Model):
+    event = models.ManyToManyField(Event, verbose_name="Która edycja", default="wybierz")
+    day = models.DateField(date.today)
+    condition = models.CharField(verbose_name="Warunek", max_length=64, default="wybierz")
+    song = models.ForeignKey('Song', on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.day
+
+
+
 
