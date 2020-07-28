8eb95fa8671581f03193d44cd549810a4d193b21
Drizi <wojtas1478@gmail.com>
2020-07-28 21:04:56 +0200

    bład w paginacji

--- challange/challange/views.py
+++ challange/challange/views.py
@@ -5,16 +5,16 @@ from django.db.models import Q
 
 
 def search_song_view(request):
-    song_list = Song.objects.all()
-    paginator = Paginator(song_list, 1)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    song_list = Song.objects.all().order_by('-id')
     query = request.GET.get("q", None)
-    qs = Song.objects.all()
+    qs = Song.objects.all().order_by('-id')
     if query is not None:
         qs = qs.filter(
             Q(title__icontains=query) | Q(artist__icontains=query)
-        )
+        ).order_by('-id')
+    paginator = Paginator(song_list, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         "song_list": qs,
         'page_obj': page_obj,
