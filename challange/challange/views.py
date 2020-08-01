dc77010b73c12c27b18f35de3eba6b124fe8c090
Drizi <wojtas1478@gmail.com>
2020-08-01 18:36:34 +0200

    dodanie wyświetlania daty, oraz sortowanie automatyczne wg. dni

--- challange/challange/views.py
+++ challange/challange/views.py
@@ -7,12 +7,12 @@ from django.db.models import Q
 
 def search_song_view(request):
     query = request.GET.get("q", None)
-    qs = Song.objects.all().order_by('-id')
+    qs = Song.objects.all().order_by('day')
     if query is not None:
         qs = qs.filter(
             Q(title__icontains=query) | Q(artist__icontains=query)
         )
-    paginator = Paginator(qs.order_by('-id'), 2)
+    paginator = Paginator(qs.order_by('day'), 2)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
