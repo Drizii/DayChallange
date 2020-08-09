f596a832d16f95aa5494d5625bec4c765271a81b
Drizi <wojtas1478@gmail.com>
2020-08-09 04:14:50 +0200

    wyświetlanie dnia po Polsku na /day/ ładniejsza wersja searchbar'a na /search/
    aktualizacja requirements : Dajango 3.7

--- challange/settings.py
+++ challange/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'challange.urls'
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-en'
 
 TIME_ZONE = 'UTC'
 
