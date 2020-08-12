c40a9c554c8c0ef39df681f672d6ea1b208531f0
Drizi <wojtas1478@gmail.com>
2020-08-12 17:26:49 +0200

    poprwka JS

--- challange/static/JS/script.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $("a").each(function () {
-        if ($(this).prop("href") == window.location.href.split("?")[0]) {
-            $(this).addClass("active");
-            $(this).parents("li").addClass("active");
-        }
-    });
-});
