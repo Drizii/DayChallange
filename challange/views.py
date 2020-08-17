fee1ae4913f020b1a62cda601f54f512bbca7654
Drizi <wojtas1478@gmail.com>
2020-08-17 18:44:25 +0200

    małysza magia

--- challange/views.py
+++ challange/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta, date
+
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
@@ -35,5 +37,34 @@ class DayListView(ListView):
 class PersonDetailView(DetailView):
     model = Person
     template_name = "person_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["days"] = self.get_days()
+        return context
+
+    def get_days(self):
+        days_count = 100
+        start_date = date(2020, 7, 27)
+        days_list = self.__get_person_days()
+        active_days = []
+        for i in range(days_count):
+            today = start_date + timedelta(days=i)
+            active_day = {
+                "date": today,
+                "active": self.__is_active(today, days_list),
+            }
+            active_days.append(active_day)
+        return active_days
+
+    def __get_person_days(self):
+        person_songs = self.object.song_set.all()
+        days_list = []
+        for song in person_songs:
+            days_list.append(song.day.date)
+        return days_list
+
+    def __is_active(self, today, days_list):
+        return today in days_list
 
 
